f9ce02c612738786f19cbd311bb6eda783c74ac0
kostik <kostik@loud.villa>
2021-05-30 23:03:44 +0300

    Add post to HASS

--- furnance.py
+++ furnance.py
@@ -4,8 +4,47 @@ import serial
 import time
 import struct
 
+import json
+from requests import get, post
+from secrets import token, host
+
 DEBUG = False
 DEBUG = True
+
+# Home
+url_temp = host + "api/states/sensor.furnance_temp"
+url_power = host + "api/states/sensor.furnance_power"
+url_state = host + "api/states/sensor.furnance_state"
+
+headers = {
+              "Authorization": token,
+              "content-type": "application/json",
+          }
+
+furnance_temp = {
+            "attributes": {
+                "friendly_name": "Furnance Temperature",
+                "icon": "mdi:thermometer",
+                "unit_of_measurement": "\u00b0C"
+            },
+            "entity_id": "sensor.furnance_temp",
+            "state": "10.0"
+}
+
+furnance_power = {
+            "attributes": {
+                "friendly_name": "Furnance Power",
+                "unit_of_measurement": "%"
+            },
+            "entity_id": "sensor.furnance_power",
+            "state": "0.0"
+}
+
+furnance_state = {
+            "attributes": { "friendly_name": "Furnance State", },
+            "entity_id": "sensor.furnance_state",
+            "state": "0"
+}
 
 #класс последовательного порта с логированием данных
 class ComPort(serial.Serial):
@@ -664,6 +703,7 @@ def Connect():
             pass
 
 def Run():
+    Trm = Connect()
     while True:
         try:
             State = 0
@@ -680,12 +720,15 @@ def Run():
                 Step = Trm.GetInt16('r.StP')
 
             if DEBUG: print('ON::State: {} Step: {} SetPoint: {} Power: {}% Temp: {}'.format(State, Step, SetPoint, Power, Temp))
+            furnance_temp['state'] = Temp
+            furnance_power['state'] = Power
+            furnance_state['state'] = State
 
+            response = post(url_temp, headers = headers, json = furnance_temp)
+            response = post(url_power, headers = headers, json = furnance_power)
+            response = post(url_state, headers = headers, json = furnance_state)
+            time.sleep(60.0)
         except Exception as e:
-            # if DEBUG: print('Retry reading', e)
-            continue
+            if DEBUG: print('Retry reading', e)
             pass
-        time.sleep(2.0)
-
-Trm = Connect()
 Run()
